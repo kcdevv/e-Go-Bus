41552b40fd3e39f5b516d9b68306c84f83c8e802
kcjod <stealgamerofficial@gmail.com>
2024-12-06 08:20:25 +0530

    fixed app.config.js

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'; // Load .env file
+import "dotenv/config"; // Load .env file
 
 export default {
   expo: {
@@ -19,7 +19,8 @@ export default {
       supportsTablet: true,
       infoPlist: {
         NSLocationWhenInUseUsageDescription: "We need your location for maps",
-        NSLocationAlwaysUsageDescription: "We need your location for maps and directions",
+        NSLocationAlwaysUsageDescription:
+          "We need your location for maps and directions",
         NSAppTransportSecurity: {
           NSAllowsArbitraryLoads: true,
         },
@@ -57,16 +58,14 @@ export default {
     },
     plugins: ["expo-router"],
     extra: {
-      firebase: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-      },
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+      FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
       router: {
         origin: false,
       },
